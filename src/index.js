abf606c9949ccd34df49e99b49b47effaf44cf80
Võ Minh Thông <thongvo109@gmail.com>
2020-11-05 15:46:08 +0700

    (feat) update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const exphbs  = require('express-handlebars');
 const app = express()
 const path = require('path');
-
 const port =3000
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname,'public')))
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname, 'resources/views'));
 
 
-
 app.get('/trang-chu', (req,res) => { res.render('home')})
+app.get('/news', (req,res) => { res.render('news')})
 
 app.listen(port,() => console.log(`Example listring ${port}`));

2506e4906fc59dee65dba18190bbcb70476864ce
Võ Minh Thông <thongvo109@gmail.com>
2020-11-06 12:11:07 +0700

    (feat) test

--- src/routes/site.js
+++ src/routes/site.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const siteController = require('../app/controllers/site_controller');
 
-router.use('/search', siteController.search);
+          router.use('/search', siteController.search);
 router.use('/', siteController.index);
 
 module.exports = router;

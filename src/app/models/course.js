f727c00016afc1ff7fab6f6aa59010567f99c67e
Vo Thong <vothong@Vos-MacBook-Pro.local>
2020-11-20 00:53:20 +0700

    (feat) update mongoose delete

--- src/app/models/course.js
+++ src/app/models/course.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
 
-mongoose.plugin(slug);
 const Course = new Schema(
     {
         name: {
@@ -32,5 +32,7 @@ const Course = new Schema(
         timestamps: true,
     },
 );
-
+// Add plug in
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete);
 module.exports = mongoose.model('Course', Course);
